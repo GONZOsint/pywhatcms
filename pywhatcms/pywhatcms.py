9bb127e6d73e3858541fc871ddd2052c95975271
umz <kumraz858@gmail.com>
2019-05-04 04:16:34 +0530

    Print output
    Prints the output in the shell and fixed whatcms.org link in readme.md

--- pywhatcms/pywhatcms.py
+++ pywhatcms/pywhatcms.py
@@ -1,13 +1,29 @@
 import requests
 
 def whatcms(key, url):
-    whatcms.r = requests.get('https://whatcms.org/APIEndpoint/Detect?key=' + key + '&url=' + url).json()
-    whatcms.name = whatcms.r['result']['name']
-    whatcms.code = whatcms.r['result']['code']
-    whatcms.confidence = whatcms.r['result']['confidence']
-    whatcms.cms_url = whatcms.r['result']['cms_url']
-    whatcms.version = whatcms.r['result']['version']
-    whatcms.msg = whatcms.r['result']['msg']
-    whatcms.id = whatcms.r['result']['id']
-    whatcms.request = whatcms.r['request']
-    whatcms.request_web = whatcms.r['request_web']
+
+    r = requests.get('https://whatcms.org/APIEndpoint/Detect?key=' + key + '&url=' + url)
+    data = r.json()
+
+    whatcms.name = data['result']['name']
+    whatcms.code = data['result']['code']
+    whatcms.confidence = data['result']['confidence']
+    whatcms.cms_url = data['result']['cms_url']
+    whatcms.version = data['result']['version']
+    whatcms.msg = data['result']['msg']
+    whatcms.id = data['result']['id']
+    whatcms.request = data['request']
+    whatcms.request_web = data['request_web']
+
+    print(f'''
+Status Code : {whatcms.code}
+Response Message : {whatcms.msg}
+
+1) CMS - {whatcms.name}
+2) CMS Version - {whatcms.version}
+3) CMS Confidence - {whatcms.confidence}
+4) CMS URL - {whatcms.cms_url}
+5) CMS ID - {whatcms.id}
+6) Request URL - {whatcms.request}
+7) https://whatcms.org Response - {whatcms.request_web}
+''')
